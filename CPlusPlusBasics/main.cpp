30e78b658e8700d011cc0a23baf427a290bdba65
andrii.slisarchuk <andrii.slisarchuk@vakoms.com.ua>
2015-10-06 01:27:24 +0300

    Added new materials.

--- CPlusPlusBasics/main.cpp
+++ CPlusPlusBasics/main.cpp
@@ -1,10 +1,63 @@
+#include <array>
+#include <memory>
 #include <climits>
 #include <typeinfo>
 #include <iostream>
+#include <algorithm>
 
 #define printTypeInfo(value) std::cout << std::boolalpha << typeid(value).name() << " " <<  #value << ": " << value << " size: " << sizeof(value) << " byte." << std::endl;
 
-int main()
+enum class Planets
+{
+    Mercury = 0,
+    Venus,
+    Earth,
+    Mars,
+    Jupiter,
+    Saturn,
+    Uranus,
+    Neptune,
+    Pluto
+};
+
+void printPlanetName(Planets planet)
+{
+    switch (planet)
+    {
+    case Planets::Mercury:
+        std::cout << "Mercury" << std::endl;
+        break;
+    case Planets::Venus:
+        std::cout << "Venus" << std::endl;
+        break;
+    case Planets::Earth:
+        std::cout << "Earth" << std::endl;
+        break;
+    case Planets::Mars:
+        std::cout << "Mars" << std::endl;
+        break;
+    case Planets::Jupiter:
+        std::cout << "Jupiter" << std::endl;
+        break;
+    case Planets::Saturn:
+        std::cout << "Saturn" << std::endl;
+        break;
+    case Planets::Uranus:
+        std::cout << "Uranus" << std::endl;
+        break;
+    case Planets::Neptune:
+        std::cout << "Neptune" << std::endl;
+        break;
+    case Planets::Pluto:
+        std::cout << "Pluto" << std::endl;
+        break;
+    default:
+        std::cout << "Wrong planet!" << std::endl;
+        break;
+    }
+}
+
+int main(int argc, char *argv[])
 {
     //Logic type
     //    bool logic {true};
@@ -26,9 +79,6 @@ int main()
     //    long long integerLongLong {4};
     //    printTypeInfo(integerLongLong);
 
-    //    int array[2] {0, 1};
-    //    printTypeInfo(array);
-
     //Float types
     //    float floatNumber {1.1};
     //    printTypeInfo(floatNumber);
@@ -36,8 +86,11 @@ int main()
     //    double doubleNumber {2.1};
     //    printTypeInfo(doubleNumber);
 
-    //    long double doubleNumberLong {3.1};
+    //    long double doubleNumberLong {3.1autoVariable};
     //    printTypeInfo(doubleNumberLong);
+
+    //    auto autoVariable = *argv;
+    //    printTypeInfo(autoVariable);
 
     //Initialize and assign operator
     //    int firstInitialize {1};
@@ -46,23 +99,34 @@ int main()
     //    int thirdInitialize = INT64_MAX + 1;
 
     //References and pointers
-    //    short hardcodedValue = 42;
+    //    auto hardcodedValue = 42;
 
-    //    short& reference = hardcodedValue;
+    //    auto& reference = hardcodedValue;
     //    printTypeInfo(reference);
 
-    //    short* pointer = &hardcodedValue;
+    //    decltype(hardcodedValue)* pointer {&hardcodedValue};
     //    printTypeInfo(pointer);
     //    printTypeInfo(*pointer);
+    //    delete pointer;         // Bad-bad delete
 
     //    void* voidPointer = pointer;
     //    printTypeInfo(voidPointer);
-    //    printTypeInfo(*((short *)voidPointer));
+    //    printTypeInfo(*((decltype(hardcodedValue)* )voidPointer));
+
+    //Smart pointers
+    //    auto uniquePtr = std::make_unique<decltype(hardcodedValue)>(hardcodedValue);
+    //    printTypeInfo(uniquePtr.get());
+    //    printTypeInfo(*uniquePtr);
+
+    //    auto sharedPtr = std::make_shared<decltype(hardcodedValue)>(hardcodedValue);
+    //    printTypeInfo(sharedPtr.get());
+    //    printTypeInfo(*sharedPtr);
+
+    //Constants
+    //    const auto marsRadius = 3396.2;
+    //    const auto earthRadius = 6378.1;
 
     //If and swich
-    //    double marsRadius = 3396.2;
-    //    double earthRadius = 6378.1;
-
     //    if(marsRadius > earthRadius)
     //    {
     //        printTypeInfo(marsRadius);
@@ -71,74 +135,53 @@ int main()
     //    {
     //        printTypeInfo(earthRadius);
     //    }
+    //    printPlanetName(Planets::Earth);
 
-    //    enum SolarSystemPlanets
+    //Arrays, for and while loop
+    //    const auto arraySize = 3;
+    //    int array[arraySize] {0, 1, 2};
+    //    printTypeInfo(array);
+    //    printTypeInfo(array[0]);
+    //    printTypeInfo(*array + 1);
+    //    printTypeInfo(2[array]);
+
+    //    int* arrayDynamic = new int[2]{2, 3};
+    //    printTypeInfo(arrayDynamic);
+    //    printTypeInfo(*arrayDynamic);
+    //    printTypeInfo(arrayDynamic[1]);
+    //    delete[] arrayDynamic;
+    //    //Raw for loop
+    //    for (int i = 0; i < arraySize; ++i)
     //    {
-    //        Mercury = 0,
-    //        Venus,
-    //        Earth,
-    //        Mars,
-    //        Jupiter,
-    //        Saturn,
-    //        Uranus,
-    //        Neptune,
-    //        Pluto,
-    //        Count
-    //    };
-
-    //    SolarSystemPlanets peopleHomeland {Earth};
-
-    //    switch (peopleHomeland) {
-    //    case Mercury:
-    //        std::cout << "Mercury" << std::endl;
-    //        break;
-    //    case Venus:
-    //        std::cout << "Venus" << std::endl;
-    //        break;
-    //    case Earth:
-    //        std::cout << "Earth" << std::endl;
-    //        break;
-    //    case Mars:
-    //        std::cout << "Mars" << std::endl;
-    //        break;
-    //    case Jupiter:
-    //        std::cout << "Jupiter" << std::endl;
-    //        break;
-    //    case Saturn:
-    //        std::cout << "Saturn" << std::endl;
-    //        break;
-    //    case Uranus:
-    //        std::cout << "Uranus" << std::endl;
-    //        break;
-    //    case Neptune:
-    //        std::cout << "Neptune" << std::endl;
-    //        break;
-    //    case Pluto:
-    //        std::cout << "Pluto" << std::endl;
-    //        break;
-    //    case Count:
-    //        std::cout << "All planets count: " << Count << std::endl;
-    //        break;
-    //    default:
-    //        std::cout << "Wrong planet!" << std::endl;
-    //        break;
-    //    }
-    //For and while loop
-    //    for (int i = 0; i < 10; ++i)
-    //    {
-    //        std::cout << i + 1 << " ";
+    //        std::cout << array[i] << " ";
     //    }
     //    std::cout << std::endl;
 
-    //    bool continueWork = true;
-    //    int counter = 10;
-    //    while(continueWork)
+    //    //Iterator for loop
+    //    for(auto it = std::begin(array); it != std::end(array); ++it)
     //    {
-    //        if(--counter < 5)
-    //        {
-    //            continueWork = false;
-    //        }
-    //        std::cout << counter << " ";
+    //        std::cout << (*it) << " ";
+    //    }
+    //    std::cout << std::endl;
+
+    //    //Range base for loop
+    //    for(const auto& element : array)
+    //    {
+    //        std::cout << element << " ";
+    //    }
+    //    std::cout << std::endl;
+
+    //    //std::algotithm for_each loop
+    //    std::for_each(std::begin(array), std::end(array),[](auto element) {
+    //        std::cout << element << " ";
+    //    });
+    //    std::cout << std::endl;
+
+    //    std::array<std::string, 3> wordsArray {"first", "second", "third"};
+    //    auto word = std::begin(wordsArray);
+    //    while(word != std::end(wordsArray))
+    //    {
+    //        std::cout << *(word++) << " ";
     //    }
     //    std::cout << std::endl;
 
